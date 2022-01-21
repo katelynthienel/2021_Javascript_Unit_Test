550b93773aefa756ba595a0e506d090adf0fa9b1
Katelyn Thienel <katelyn.thienel@vtsd.com>
2022-01-21 07:58:32 -0500

    javascript update!

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,6 +1,8 @@
 /* Put code for the task below */
-function functionName(){
-	alert("Run My Stuff");
-	
+///function functionName(){
+	///alert("Run My Stuff");
+function Task1(){
+	alert("task 1");
+
 	
 }
