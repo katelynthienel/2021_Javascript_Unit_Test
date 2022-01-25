99f456761df142dd694489905110c191e812b24c
Katelyn Thienel <katelyn.thienel@vtsd.com>
2022-01-25 10:37:30 -0500

    javascript update!

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,14 +1,14 @@
 /* Put code for the task below */
 function red() {
-    var supriseImage = document.getElementById("red");
-    supriseImage.style.display = "block";
+    var supriseImage = document.getElementById("table2");
+    supriseImage.style.background = "red";
 }
 function blue() {
-    var supriseImage = document.getElementById("blue");
-    supriseImage.style.display = "block";
+    var supriseImage = document.getElementById("table2");
+    supriseImage.style.background = "blue";
 }
 function green() {
-    var supriseImage = document.getElementById("green");
-    supriseImage.style.display = "block";
+    var supriseImage = document.getElementById("table2");
+    supriseImage.style.background = "green";
 }
 
